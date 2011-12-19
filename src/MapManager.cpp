c889dde2a60362ec506ea1485be65be9302596f5
paulpetit.mel <paulpetit.mel@localhost>
2011-12-19 14:45:30 +0100

    Premi?re ?bauche de g?n?ration de map.

--- src/MapManager.cpp
+++ src/MapManager.cpp
@@ -25,10 +25,54 @@ LOG("call MapManager destructor");
 
 std::vector<std::pair<Triplet,PhysicalMaterial> > MapManager::loadChunk(Triplet chunkPosition)
 {
-	std::vector<std::pair<Triplet,PhysicalMaterial> > result = std::vector<std::pair<Triplet,PhysicalMaterial> > ();
 	std::string filename = "map/x" + Ogre::StringConverter::toString(int(chunkPosition.x)) +
-							"y" + Ogre::StringConverter::toString(int(chunkPosition.y)) +
-							"z" + Ogre::StringConverter::toString(int(chunkPosition.z));
+						   "y" + Ogre::StringConverter::toString(int(chunkPosition.y)) +
+						   "z" + Ogre::StringConverter::toString(int(chunkPosition.z));
+	std::ifstream chunk(filename.c_str());
+	if (chunk) { // Le fichier existe
+		chunk.close();
+		return loadChunkFromFile(chunkPosition, filename);
+	}
+	return generateChunk(chunkPosition);
+}
+
+std::vector<std::pair<Triplet,PhysicalMaterial> > MapManager::generateChunk(Triplet chunkPosition)
+{
+	std::vector<std::pair<Triplet,PhysicalMaterial> > result = std::vector<std::pair<Triplet,PhysicalMaterial> > ();
+
+	int altitude = 0;
+//	PhysicalMaterial material;
+	for (int i=0; i<mChunkSize; i++) {
+		for (int j=0; j<mChunkSize; j++) {
+			if (rand()%2) {
+				// On change l'altitude
+				if (rand()%2)
+					altitude++;
+				else
+					altitude--;
+			}
+			for (int k=-10; k<altitude; k++) {
+				if (k >= 4) {
+					if (rand()%4)
+						result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i, chunkPosition.z * mChunkSize + k, chunkPosition.z * mChunkSize +j), PhysicalMaterial::Dirt));
+					else
+						result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i, chunkPosition.z * mChunkSize + k, chunkPosition.z * mChunkSize +j), PhysicalMaterial::Grass));
+				}
+				else if (k<0){
+					result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i, chunkPosition.z * mChunkSize + k, chunkPosition.z * mChunkSize +j), PhysicalMaterial::Dirt));
+				}
+				else {
+					result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i, chunkPosition.z * mChunkSize + k, chunkPosition.z * mChunkSize +j), PhysicalMaterial::Grass));
+				}
+			}
+		}
+	}
+	return result;
+}
+
+std::vector<std::pair<Triplet,PhysicalMaterial> > MapManager::loadChunkFromFile(Triplet chunkPosition, std::string filename) {
+	std::vector<std::pair<Triplet,PhysicalMaterial> > result = std::vector<std::pair<Triplet,PhysicalMaterial> > ();
+
 	std::ifstream chunk(filename.c_str());
 	int i = 0;
 	int k = 0;
@@ -42,14 +86,14 @@ std::vector<std::pair<Triplet,PhysicalMaterial> > MapManager::loadChunk(Triplet 
 		}
 		else if( c == ' ')
 		{
-			result.push_back(std::pair<Triplet,PhysicalMaterial>(Ogre::Vector3(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize , chunkPosition.z * mChunkSize +k), PhysicalMaterial::Grass));
+			result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize , chunkPosition.z * mChunkSize +k), PhysicalMaterial::Grass));
 			i++;
 		}
 		else if( c == 'd')
 		{
-			result.push_back(std::pair<Triplet,PhysicalMaterial>(Ogre::Vector3(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize , chunkPosition.z * mChunkSize +k), PhysicalMaterial::Grass));
-			result.push_back(std::pair<Triplet,PhysicalMaterial>(Ogre::Vector3(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize + 1 , chunkPosition.z * mChunkSize +k), PhysicalMaterial::Dirt));
-			result.push_back(std::pair<Triplet,PhysicalMaterial>(Ogre::Vector3(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize + 2, chunkPosition.z * mChunkSize +k), PhysicalMaterial::Dirt));
+			result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize , chunkPosition.z * mChunkSize +k), PhysicalMaterial::Grass));
+			result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize + 1 , chunkPosition.z * mChunkSize +k), PhysicalMaterial::Dirt));
+			result.push_back(std::pair<Triplet,PhysicalMaterial>(Triplet(chunkPosition.x * mChunkSize + i,chunkPosition.y * mChunkSize + 2, chunkPosition.z * mChunkSize +k), PhysicalMaterial::Dirt));
 			i++;
 		}
 	}
